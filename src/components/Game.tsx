516461164e6868e344c3998edb2c6b370d4985d3
AlbertLnz <albert2000_lanza@hotmail.com>
2024-04-08 09:12:18 +0200

    Show events when user does a gesture on mobile

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 import { Colors } from '../styles/colors'
+import { PanGestureHandler } from 'react-native-gesture-handler'
 
 export default function Game():JSX.Element {
+  
+  const handlerGesture = (event: any) => {
+    console.log(event.nativeEvent)
+  }
+  
   return (
-    <SafeAreaView style={styles.container}></SafeAreaView>
+    <PanGestureHandler onGestureEvent={handlerGesture}>
+      <SafeAreaView style={styles.container}></SafeAreaView>
+    </PanGestureHandler>
   )
 }
 
