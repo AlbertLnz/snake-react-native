26d863b0ad9202ac278652c6de5b97f607e35aa9
AlbertLnz <albert2000_lanza@hotmail.com>
2024-04-08 08:47:28 +0200

    Involve App.tsx to GestureHandlerRootView of React Native Gesture Handler

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 import Game from "./src/components/Game"
+import 'react-native-gesture-handler'
 
 const App = () => {
-  <Game />
+  <GestureHandlerRootView style={{ flex: 1}}>
+    <Game />
+  </GestureHandlerRootView>
 }
 
 export default App
